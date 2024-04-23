4b3d030fe1078e21b678ef8faaef9c0ad6c7c740
John Raymon Macalib-og <johnraymon.macalib-og@cosmotech.com.ph>
2024-04-23 18:18:32 +0800

    added different color to add client

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -306,7 +306,7 @@ const LandingPage = () => {
         <div className='place-items-center'>
           <button type="button"
             onClick={handleAddModalOpen}
-            class="m-10 px-4 py-2.5 flex items-center text-[#fff] rounded-full text-sm font-semibold outline-none transition-all bg-slate-600 hover:bg-indigo-700 active:bg-slate-600">
+            class="m-10 px-4 py-2.5 flex items-center text-[#fff] rounded-full text-sm font-semibold outline-none transition-all bg-slate-600 hover:bg-slate-700 active:bg-slate-600">
             <svg xmlns="http://www.w3.org/2000/svg" width="18px" fill="currentColor" class="mr-2" viewBox="0 0 6.35 6.35">
               <path fill-rule="evenodd" d="M3.181.264A2.92 2.92 0 0 0 .264 3.18a2.922 2.922 0 0 0 2.917 2.917A2.92 2.92 0 0 0 6.096 3.18 2.919 2.919 0 0 0 3.18.264zm0 .53A2.38 2.38 0 0 1 5.566 3.18 2.382 2.382 0 0 1 3.18 5.566 2.384 2.384 0 0 1 .794 3.179 2.383 2.383 0 0 1 3.181.794zm-.004 1.057a.265.265 0 0 0-.263.27v.794h-.793a.265.265 0 0 0-.028 0 .266.266 0 0 0 .028.53h.793v.794a.265.265 0 0 0 .531 0v-.793h.794a.265.265 0 0 0 0-.531h-.794v-.794a.265.265 0 0 0-.268-.27z" data-original="#000000" paint-order="stroke fill markers" />
             </svg>
@@ -427,7 +427,7 @@ const LandingPage = () => {
         </div>
         {/* <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-8 text-center mt-12"> */}
         <div className='w-full overflow-x-auto'>
-  <table className='w-full table-auto'>
+  <table className='w-full table-striped'>
     {clientData && clientData.map((clientInfo) => {
       // console.log(clientInfo)
       return (
